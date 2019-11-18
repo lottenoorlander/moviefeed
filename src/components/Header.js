b27f2139e3fd3b30d057069b4f47c2cba7431382
Jacque Joosten <jacque.joosten@bnnvara.nl>
2019-11-18 18:01:43 +0100

    added likes component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Likes from "./Likes";
 
 export default class Header extends Component {
   render() {
@@ -7,6 +8,7 @@ export default class Header extends Component {
         <h1>{this.props.title}</h1>
         <span>{this.props.releaseDate}</span>
         <img src={this.props.imageUrl} alt={this.props.imageAlt} />
+        <Likes imageUrl="placeholdergit" />
       </header>
     );
   }
