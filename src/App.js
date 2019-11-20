faf89dae26b81e66ca951213a86074f30ea915b9
Jacque Joosten <jacque.joosten@bnnvara.nl>
2019-11-20 16:06:53 +0100

    solved conflicts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
 import MovieFeed from "./components/MovieFeed";
-=======
 import Likes from "./components/Likes";
 import AddMovie from "./components/AddMovie";
->>>>>>> develop
 
 function App() {
   return (
     <div className="container">
       <Navbar href="wwww.imdb.com/" />
-<<<<<<< HEAD
       <header className="App-header"></header>
       <MovieFeed />
-=======
       <Likes />
       <AddMovie />
->>>>>>> develop
     </div>
   );
 }
